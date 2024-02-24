04ea82cd2ede0293a8d4bb10d871c058a4aad9b2
Hugo Adrián Meza Vega <hamv15@hotmail.com>
2024-02-23 20:04:45 -0600

    Initial Commit

---
+++ MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by Hugo Adrián Meza Vega on 23/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
