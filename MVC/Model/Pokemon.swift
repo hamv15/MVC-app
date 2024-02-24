8e56776f52e8c86826dafa0cb5f574782f7efc2b
Hugo Adrián Meza Vega <hamv15@hotmail.com>
2024-02-24 14:13:32 -0600

    Finalización de la práctica uno la cual consiste en la construcicón de un table view que lleva a un view controller para mostrar información detallada del elemento seleccionado en el table view. Se agrega botón de cerrar modal y el cambio de colores de fuente al activar el modo oscuro.

---
+++ MVC/Model/Pokemon.swift
@@ -0,0 +1,8 @@
+//
+//  Pokemon.swift
+//  MVC
+//
+//  Created by Hugo Adrián Meza Vega on 23/02/24.
+//
+
+import Foundation
---
+++ MVC/Model/PokemonDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonDataManager.swift
+//  MVC
+//
+//  Created by Hugo Adrián Meza Vega on 23/02/24.
+//
+
+import Foundation
